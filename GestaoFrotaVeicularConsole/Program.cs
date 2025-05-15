0a57959a2120b6995cadd3a10fa6a0fbd6d5f85d
Vagner Nogueira <vagner.nogueira@gmail.com>
2025-05-14 23:27:38 -0300

    Update program.cs

--- GestaoFrotaVeicularConsole/Program.cs
+++ GestaoFrotaVeicularConsole/Program.cs
@@ -2,17 +2,17 @@
 using System.Xml.Linq;
 using GestaoFrotaVeicularConsole;
 
-Dictionary<string, Vehicle> vehicleList = new Dictionary<string, Vehicle>();
+Dictionary<string, VehicleType> vehicleTypeList = new Dictionary<string, VehicleType>();
 
 bool exit = false;
 
 while (!exit)
 {
-    Console.WriteLine("Bem vindo ao Gestão de Frota Veicular!");
-    Console.WriteLine("Digite 1 para registrar um Veículo");
-    Console.WriteLine("Digite 2 para registrar um Tipo de Veículo");
-    Console.WriteLine("Digite 3 para mostrar todos os Veículo");
-    Console.WriteLine("Digite 4 para mostrar os Veículo de um Tipo de Veículo");
+    Console.WriteLine("Bem vindo ao Gestão de Frota Veicular!\n");
+    Console.WriteLine("Digite 1 para registrar um Tipo de Veículo");
+    Console.WriteLine("Digite 2 para registrar um Veículo");
+    Console.WriteLine("Digite 3 para mostrar todos os Veículos");
+    Console.WriteLine("Digite 4 para mostrar os Veículos de um Tipo de Veículo");
     Console.WriteLine("Digite -1 para sair :(");
 
     Console.WriteLine("Informe sua opção");
@@ -21,7 +21,16 @@ while (!exit)
     switch (option)
     {
         case 1:
+            VehicleTypeRegistration();
+            break;
+        case 2:
             VehicleRegistration();
+            break;
+        case 3:
+            VehicleGet();
+            break;
+        case 4:
+            VehicleTypeGet();
             break;
         case -1:
             Console.Clear();
@@ -36,7 +45,65 @@ while (!exit)
     Console.Clear();
 }
 
+void VehicleTypeGet()
+{
+    Console.Clear();
+    Console.WriteLine("Exibir Veículos de um Tipo de Veículo");
+    Console.WriteLine("Digite o Tipo de Veículo");
+    string name = Console.ReadLine();
+    if (vehicleTypeList.ContainsKey(name))
+    {
+        VehicleType vehicleType = vehicleTypeList[name];
+        vehicleType.showVeicles();
+    }
+    else
+    {
+        Console.WriteLine($"O Tipo de Veículo {name} não existe");
+    }
+}
+
+void VehicleGet()
+{
+    Console.Clear();
+    Console.WriteLine("Lista de Veículos:");
+    foreach (var item in vehicleTypeList.Values)
+    {
+        item.showVeicles();
+    }
+}
+
 void VehicleRegistration()
 {
-    throw new NotImplementedException();
+    Console.Clear();
+    Console.WriteLine("Registro de Veículos");
+    Console.WriteLine("Digite a Marca/Modelo do Veículo que você deseja cadastrar");
+    string markModel = Console.ReadLine();
+    Console.WriteLine("Digite o Ano do Veículo que você deseja cadastrar");
+    int year = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a Placa do Veículo que você deseja cadastrar");
+    string plate = Console.ReadLine();
+    Vehicle vehicle = new(markModel, year, plate);
+    Console.WriteLine("Digite o Nome do Tipo de Veículo do Veículo que você deseja cadastrar");
+    string vehicleTypeName = Console.ReadLine();
+    if (vehicleTypeList.ContainsKey(vehicleTypeName))
+    {
+        vehicleTypeList[vehicleTypeName].addVehicle(vehicle);
+    }
+    else
+    {
+        Console.WriteLine($"O Tipo de Veículo {vehicleTypeName} não existe.");
+    }
+    Console.WriteLine($"Veículo {plate} adcionado com sucesso!");
+}
+
+void VehicleTypeRegistration()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de Tipo de Veículo");
+    Console.WriteLine("Digite o Nome do Tipo de Veículo que você deseja cadastrar");
+    string name = Console.ReadLine();
+    Console.WriteLine("Digite a Descrição do Tipo de Veículo que você deseja cadastrar");
+    string description = Console.ReadLine();
+    VehicleType vehicleType = new(name, description);
+    vehicleTypeList.Add(name, vehicleType);
 }
