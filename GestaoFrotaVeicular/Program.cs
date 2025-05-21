f80b9a6a617bb3513251dc537a15b7f951d3a6fa
Vagner Nogueira <vagner.nogueira@gmail.com>
2025-05-21 18:20:57 -0300

    GET method configured

--- GestaoFrotaVeicular/Program.cs
+++ GestaoFrotaVeicular/Program.cs
@@ -1,6 +1,17 @@
+using GestaoFrotaVeicular.Shared.Data.DB;
+using GestaoFrotaVeicular.Shared.Models;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.SerializerOptions.WriteIndented = true;
+});
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", () =>
+    {
+        var dal = new DAL<Vehicle>();
+        return dal.Read();
+    });
 app.Run();
